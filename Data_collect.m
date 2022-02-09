2a0cc7c678fd713f67207a62e2f40e3c3ebf6191
Andy <35400540+AndyHML@users.noreply.github.com>
2022-02-09 02:41:21 +0100

    Add files via upload

--- Data_collect.m
+++ Data_collect.m
@@ -6,7 +6,7 @@ pause(0.05);
 m1.isStreaming;
 %m1.stopStreaming();
 
-%meanEMG = mean(m1.emg_log, 2);
+%meanEMG = mean(m1.emg_log, 2); %Calculate mean of 8 channels by row
 %plot(m1.timeEMG_log, meanEMG), title('EMG')
 
 %clear %Clear the old session
