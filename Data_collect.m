41afc3c822012dd37429d155abcf55632cf40df5
Andy <35400540+AndyHML@users.noreply.github.com>
2022-02-10 13:28:16 +0100

    Add files via upload

--- Data_collect.m
+++ Data_collect.m
@@ -5,8 +5,12 @@ m1 = mm.myoData(1);
 pause(0.05);
 m1.isStreaming;
 %m1.stopStreaming();
+%save m1
 
+%channels = stackedplot(m1.emg_log); %Plot each channel
+%channels.DisplayLabels = {"Channel 1", "Channel 2", "Channel 3", "Channel
+%4", "Channel 5", "Channel 6", "Channel 7", "Channel 8"};
 %meanEMG = mean(m1.emg_log, 2); %Calculate mean of 8 channels by row
-%plot(m1.timeEMG_log, meanEMG), title('EMG')
+%plot(m1.timeEMG_log, meanEMG), title('Mean EMG') %Plot the average of channels 
 
 %clear %Clear the old session
